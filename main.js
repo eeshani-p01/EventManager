ec711292f78c442d01404578b37f2ea8fe4a88f2
Eeshani Patel <eeshani.p01@gmail.com>
2017-11-16 18:42:13 +0530

    all Comment added

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ eventApp.controller('homeControl',function($scope,$http){
 eventApp.controller('eventControl',function($scope,$routeParams,$http){
 	var id = $routeParams.id;
 	$scope.eventDetails = [];
+	$scope.comments=[];
 	$http({
 		'method' : 'GET',
 		'url' : 'http://eventmanager-server.herokuapp.com/events',
@@ -58,13 +59,10 @@ eventApp.controller('eventControl',function($scope,$routeParams,$http){
 	},function(xhr){
 		console.log(xhr)
 	})
-})
-
-eventApp.controller('addeventControl',function($scope,$http){
-	// $scope.sendata=function()
+	var url='http://eventmanager-server.herokuapp.com/events/'+id+'?_embed=comments';
 	$http({
-		'method' : 'POST',
-		'url' : 'http://eventmanager-server.herokuapp.com/events',
+		'method' : 'GET',
+		'url' : url,
 		'headers': 'Content-Type: application/json\n',
 		"headerData": [
 					{
@@ -75,8 +73,46 @@ eventApp.controller('addeventControl',function($scope,$http){
 					}
 				]
 	}).then(function(response){
-		$scope.events = response.data;
+		$scope.comments = response.data.comments;
+		console.log(response.data.comments[0].id)
+		// console.log('comments')
 	},function(xhr){
 		console.log(xhr)
 	})
+})
+
+eventApp.controller('addeventControl',function($scope,$http){
+	$scope.sendata=function(){
+	// console.log("hello")
+		var title =  $('.dialog #title').val();
+		var date =  $('.dialog #date').val();
+		var prize =  $('.dialog #cost').val();
+		var org =  $('.dialog #host').val();
+		var data1={
+			"title":title,
+			"date":date,
+			"price":prize,
+			"organiser":org
+		}
+		console.log(data1)
+
+			$http({
+				'method' : 'POST',
+				'url' : 'http://eventmanager-server.herokuapp.com/events',
+				'headers': 'Content-Type: application/json\n',
+				"headerData": [
+							{
+								"key": "d681cbb9-fb83-1f2e-746a-57da0f33ef98",
+								"value": "application/json",
+								"description": "",
+								"enabled": true
+							}
+						],
+				'data':data1
+			}).then(function(response){
+				$scope.events = response.data;
+			},function(xhr){
+				console.log(xhr)
+			})
+	}
 })
